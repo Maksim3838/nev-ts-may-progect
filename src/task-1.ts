90bf583e5105f1e298a3968c94906576fb22cffc
Maksim <70702008m@gmail.com>
2026-03-27 19:15:34 +0200

    text

---
+++ src/task-1.ts
@@ -0,0 +1,23 @@
+
+const name = "Alice";
+  const  age = 30;
+  const  isOnline = true;
+
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
+
+
+
+const llalalal: { name: string; age: number; isOnline:boolean} = {
+     name : "rgthfdjfhgn",
+    age:  30,
+    isOnline : true,
+
+}
+
+
+console.log(`Name: ${llalalal.name}`);
+console.log(`Age: ${llalalal.age}`);
+console.log(`Online: ${llalalal.isOnline}`);
